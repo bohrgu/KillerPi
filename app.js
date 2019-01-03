1444eba6794f938b8fd7877a1783272793d23292
Guillaume Bohr <guillaume.bohr@gmail.com>
2019-01-03 02:40:37 +0100

    Fix init scripts. And fix myLog import from app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var helmet = require('helmet')
-const myLog = require('../utils/myLog')
+const myLog = require('./utils/myLog')
 
 var indexRouter = require('./routes/indexRouter')
 var gameRouter = require('./routes/gameRouter')
